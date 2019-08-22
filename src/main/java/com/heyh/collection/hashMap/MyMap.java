591283098c2a39d65b0b709fd4130724fe26107c
heyh <heyh@heyhdeMacBook-Pro.local>
2019-08-22 11:44:51 +0800

    第四次提交，HashMap实现样例

--- src/main/java/com/heyh/collection/hashMap/MyMap.java
+++ src/main/java/com/heyh/collection/hashMap/MyMap.java
@@ -1,4 +1,20 @@
 package com.heyh.collection.hashMap;
 
-public interface MyMap {
+/**
+ * 定义一个接口，对外暴露快速存取的方法
+ *
+ * 内部定义接口 Entry
+ * @param <K>
+ * @param <V>
+ */
+public interface MyMap<K, V> {
+
+    public V put(K k, V v);
+    public V get(K k);
+
+    interface Entry<K, V> {
+        public K getKey();
+        public V getValue();
+    }
+
 }
--- src/test/java/com/heyh/collection/hashMap/HashMapTest.java
+++ src/test/java/com/heyh/collection/hashMap/HashMapTest.java
@@ -1,11 +1,21 @@
 package com.heyh.collection.hashMap;
 
+import com.heyh.collection.hashMap.impl.MyHashMap;
 import org.junit.Test;
 
 public class HashMapTest {
 
     @Test
     public void test1() {
+        MyHashMap<String, String> myHashMap = new MyHashMap<>();
+        for (int i = 0; i < 10; i++) {
+            myHashMap.put("key: " + i, "value: " + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("key " + i + ", value is: " + myHashMap.get("key: " + i));
+        }
+
 
     }
 
